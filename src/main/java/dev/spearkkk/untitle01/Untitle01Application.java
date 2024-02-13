16e1c7d4d0dd37ba3a3064767d3ee7688cf1ac57
spearkkk <spearkkp@gmail.com>
2024-02-13 13:58:48 +0900

    Initialize

---
+++ src/main/java/dev/spearkkk/untitle01/Untitle01Application.java
@@ -0,0 +1,13 @@
+package dev.spearkkk.untitle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Untitle01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Untitle01Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/spearkkk/untitle01/Untitle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spearkkk.untitle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Untitle01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
